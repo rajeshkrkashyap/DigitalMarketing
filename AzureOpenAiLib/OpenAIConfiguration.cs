1b90b17197877cec836357ff7364bca5b98d839f
Rajesh Kumar Kashyap <34347460+rajeshkrkashyap@users.noreply.github.com>
2025-09-24 18:40:47 +0530

    Update OpenAIConfiguration.cs

--- AzureOpenAiLib/OpenAIConfiguration.cs
+++ AzureOpenAiLib/OpenAIConfiguration.cs
@@ -10,8 +10,8 @@ namespace AzureOpenAiLib
     // Infrastructure Layer
     public class OpenAIConfiguration
     {
-        public string ApiKey { get { return "5e9c2375881a425dbcb2d0c796da6f68";  } }
-        public string Endpoint { get { return "https://connecttoai-openai.openai.azure.com"; } }
+        public string ApiKey { get { return "api kEY";  } }
+        public string Endpoint { get { return "ADD END POINT"; } }
 
         // Other configuration settings
     }
